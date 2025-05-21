641de4ca8578c537e28320ff58c09bf4047a0b6b
stfyang <tgb090312345@gmail.com>
2025-05-22 00:43:04 +0800

    5.21.0.1

---
+++ jump.js
@@ -0,0 +1,21 @@
+function 點餐() {
+    window.location.href = "oder.html"; 
+  }
+  function 菜單() {
+    window.location.href = "menu.html"; 
+  }
+  function 進貨() {
+    window.location.href = "in.html"; 
+  }
+  function 庫存() {
+    window.location.href = "sto.html"; 
+  }
+  function 供應商() {
+    window.location.href = "idn.html"; 
+  }
+  function 營運分析() {
+    window.location.href = "an.html"; 
+  }
+  function 首頁() {
+    window.location.href = "index.html"; 
+  }
